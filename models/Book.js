5272c70314e52a32c332f1319d597fdfbbc6128e
Darius Hay <dariushay@gmail.com>
2021-05-15 00:24:48 -0400

    description not required

--- models/Book.js
+++ models/Book.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
     title: { type: String, required: true },
     author: String, 
-    description: { type: String, required: true },
+    description: String,
     image: String,
     link: String,
     date: { type: Date, default: Date.now }
